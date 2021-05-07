ce17094896ddcbddce72d6ed70b12cfe5faa6e23
Pyusuu <40074000+Restackable@users.noreply.github.com>
2021-05-07 14:12:12 -0700

    Updated SendMessage.php

--- PHP Files/SendMessage.php
+++ PHP Files/SendMessage.php
@@ -1,22 +1,45 @@
 <?php
 
+$key = $_GET['key'];
 $user = $_GET['username'];
 $message = $_GET['message'];
+
 $date = date("Y/m/d");
 $file = "msg.txt";
+
+$validkey = "0123456789";
+
+if (is_numeric($key))
+{
+    if ($key == $validkey)
+    {
+        if (isset($key))
+        {
+            
+        } else {
+            die("Unknown Error!");
+        }
+    } else {
+        die("Invalid Key!");
+    }
+} else {
+    die("Key must not be empty and must contain only numbers!");
+}
 
 if(is_file($file))
 {
 
     $file = fopen($file, 'a+') or die("Error!");
-    fwrite($file, "\n$user: $message");
+    echo("Message Sent!");
+    fwrite($file, "\n   [$date] $user: $message");
 
-}else
+}
+else
 {
 
     $file = fopen($file, 'w') or die("Error!");
-    fwrite($file, "$user: $message");
+    fwrite($file, "   [$date] $user: $message");
+    echo("Message Sent!");
     fclose($file);
-
 }
 ?>
